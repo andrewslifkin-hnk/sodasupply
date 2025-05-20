89dcacce0b2891143a5537b932b5280ae651b59f
Andrew Slifkin <slifkin@gmail.com>
2025-05-20 13:11:22 +0200

    Installed vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { FilterProvider } from "@/context/filter-context"
 import { OrderProvider } from "@/context/orders-context"
 import { Suspense } from "react"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -39,6 +40,7 @@ export default function RootLayout({
           </CartProvider>
         </StoreProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
