ea65623fdf4b32c5cd7b52d1d93f8091ef6373b9
Andrew Slifkin <slifkin@gmail.com>
2025-05-20 10:49:42 +0200

    Add Vercel Analytics to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { StoreProvider } from "@/context/store-context"
 import { FilterProvider } from "@/context/filter-context"
 import { OrderProvider } from "@/context/orders-context"
 import { Suspense } from "react"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             </OrderProvider>
           </CartProvider>
         </StoreProvider>
+        <Analytics />
       </body>
     </html>
   )
